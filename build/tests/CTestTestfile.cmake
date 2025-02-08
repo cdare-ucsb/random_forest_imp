cdcec49c74b894f8fd4694a4e6eb6cebd2567dc0
cdare77 <cdare77@vt.edu>
2025-02-07 16:19:54 -0800

    Added a class Series which allows the variant type to be tracked, as well as perform column-level adjustments in a more object-oriented manner

--- build/tests/CTestTestfile.cmake
+++ build/tests/CTestTestfile.cmake
@@ -6,4 +6,3 @@
 # testing this directory and lists subdirectories to be tested as well.
 include("/Users/chris/Documents/SourceCode/c++/random_forest_imp/build/tests/Node_tests[1]_include.cmake")
 include("/Users/chris/Documents/SourceCode/c++/random_forest_imp/build/tests/DataFrame_tests[1]_include.cmake")
-include("/Users/chris/Documents/SourceCode/c++/random_forest_imp/build/tests/DecisionTree_tests[1]_include.cmake")
